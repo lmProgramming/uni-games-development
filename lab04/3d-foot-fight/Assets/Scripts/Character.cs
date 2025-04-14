d21b4d5993474eebffffc214f5ba2bacdbd5224a
lmProgramming <mikikubs@gmail.com>
2025-04-14 10:36:30 +0200

    init: lab04

---
+++ lab04/3d-foot-fight/Assets/Scripts/Character.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private CharacterController _controller;
+
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        
+        _controller.Move(move * Time.deltaTime);
+    }
+}
